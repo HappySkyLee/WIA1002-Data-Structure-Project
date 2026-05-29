a4bdf378e1e329d52b50393a3112ac2b5f815446
HappySkyLee <happyskyleekh@gmail.com>
2026-05-29 23:55:50 +0800

    Borrow History

---
+++ src/BorrowHistory.java
@@ -0,0 +1,19 @@
+public class BorrowHistory {
+    private int isbn;
+    private String title;
+    private String author;
+
+    public BorrowHistory(Book book) {
+        this.isbn = book.getIsbn();
+        this.title = book.getTitle();
+        this.author = book.getAuthor();
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void displayRecord() {
+        System.out.println(isbn + " | " + title + " | " + author);
+    }
+}
